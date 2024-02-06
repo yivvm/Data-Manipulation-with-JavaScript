ac246446a26c445a76b39e72861af68bd8bd2da6
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-06 09:50:27 -0500

    add console log statement for each check

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+// console.log(isValid);
+console.log(`The four numbers are valid according to the provided criteria: ${isValid}`)
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -53,12 +54,14 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 // Check if all numbers are divisible by 5. 
 // Cache the result in a variable.
 const isDivide5 = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 % 5 == 0);
-console.log(isDivide5);
+// console.log(isDivide5);
+console.log(`All numbers are divisible by 5: ${isDivide5}`);
 
 // Check if the first number is larger than the last. 
 // Cache the result in a variable.
 const firstLargerLast = (n1 > n4);
-console.log(firstLargerLast);
+// console.log(firstLargerLast);
+console.log(`The first number is larger than the last: ${firstLargerLast}`);
 
 // Accomplish the following arithmetic chain:
 // Subtract the first number from the second number.
@@ -67,8 +70,11 @@ console.log(firstLargerLast);
 const secondSubtractOne = n2 - n1;
 const multiplyThird = secondSubtractOne * n3;
 const divideFourth = multiplyThird / n4;
-console.log(divideFourth);
+// console.log(divideFourth);
+console.log(`The result after accomplishing the following arithmetic chain is: ${divideFourth}`);
 
 // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 const isNotOver25 = n1 <= 25 && n2 <= 25 && n3 <= 25 && n4 <= 25;
-console.log(isNotOver25);
+// console.log(isNotOver25);
+console.log(`No number larger than 25: ${isNotOver25}`);
+
