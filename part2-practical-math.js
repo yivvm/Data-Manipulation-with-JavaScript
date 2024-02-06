9b92710544e1d44ec82aa1af40206a2caaa4a63a
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-06 09:53:42 -0500

    update questions for part 2

---
+++ part2-practical-math.js
@@ -0,0 +1,21 @@
+/*
+
+Part 2: Practical Math
+Let’s look at a more practical scenario.
+You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+How many gallons of fuel will you need for the entire trip?
+Will your budget be enough to cover the fuel expense?
+How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+Log the results of your calculations using string concatenation or template literals.
+
+*/
+
