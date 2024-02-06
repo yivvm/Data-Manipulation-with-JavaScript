be8d24763242986633a1a51b387e91b51f4f96e7
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-06 09:40:31 -0500

    update question 1, divisible by 5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Part 1: Math Problems
 // The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
@@ -8,23 +9,27 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
+// console.log(isSum50);
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+// console.log(isTwoOdd);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+// console.log(isOver25);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+// console.log(isUnique);
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -43,3 +48,10 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+// Check if all numbers are divisible by 5. 
+// Cache the result in a variable.
+const isDivide5 = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 % 5 == 0);
+console.log(isDivide5);
+
